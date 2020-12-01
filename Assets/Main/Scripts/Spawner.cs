b2b2c59dd0e07943990ceeed9aba26498b537a7f
Королев Даниил <kdvikt@gmail.com>
2020-12-01 21:10:34 +0400

    simple spawner

---
+++ Assets/Main/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform SpawnPosition;
+    public GameObject SpawnObject;
+    public float SpawnRate;
+    public int CountSpawnObjects;
+
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        for(var i = 0; i < CountSpawnObjects; i++)
+        {
+            yield return new WaitForSeconds(SpawnRate);
+            Instantiate(SpawnObject, SpawnPosition.position, Quaternion.identity);
+        }
+    }
+}
