8da593e9eaa1c9e30254ae53a7742a963f17dc9c
Королев Даниил <kdvikt@gmail.com>
2020-12-01 22:22:34 +0400

    SpawnRule and IGamePool

---
+++ Assets/Main/Scripts/FakeGamePool.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeGamePool : MonoBehaviour, IGamePool<GameObject>
+{
+    public GameObject CloneParrent;
+
+    public GameObject Spawn(Vector3 position, Quaternion quaternion)
+    {
+        return Instantiate(CloneParrent, position, quaternion);
+    }
+}
--- Assets/Main/Scripts/Spawner.cs
+++ Assets/Main/Scripts/Spawner.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public Transform SpawnPosition;
-    public GameObject SpawnObject;
-    public float SpawnRate;
-    public int CountSpawnObjects;
+   // public Transform SpawnPosition; 
+    public List<SpawnRule> SpawnRules = new List<SpawnRule>();
+
+    public IGamePool<GameObject> GamePool;
 
     void Start()
     {
-        StartCoroutine(Spawn());
+        // Каким-то нормальным образом надо сделать передачу GamePool
+        // GetComponent<IGamePool<GameObject>>() не работает, хотя на сцене есть объект реализующий этот интерфейс
+        // Так же интерфейс не отображается для интерактивной передачи реализации интерфейса в юнити (путем перетаскивания которое)
+        
+        foreach(var spawnRule in SpawnRules)
+        {
+            GamePool = new FakeGamePool()
+            {
+                CloneParrent = spawnRule.gameObject
+            };
+            if (spawnRule.SpawnRateSeconds != 0 || spawnRule.CountSpawnObjects <= 0)
+                StartCoroutine(Spawn(spawnRule));
+        }
     }
 
-    IEnumerator Spawn()
+    IEnumerator Spawn(SpawnRule spawnRule)
     {
-        for(var i = 0; i < CountSpawnObjects; i++)
+        yield return new WaitForSeconds(spawnRule.SpawnShiftSeconds);
+        for(var i = 0; i < spawnRule.CountSpawnObjects; i++)
         {
-            yield return new WaitForSeconds(SpawnRate);
-            Instantiate(SpawnObject, SpawnPosition.position, Quaternion.identity);
+            yield return new WaitForSeconds(spawnRule.SpawnRateSeconds);
+            GamePool.Spawn(this.transform.position, Quaternion.identity);
         }
     }
 }
