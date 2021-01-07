882915a9e79ebcf2ae7debab92fd8b365c2ccf76
Королев Даниил <kdvikt@gmail.com>
2021-01-07 21:07:26 +0400

    Управление мышью для теста NavMesh

---
+++ Assets/Features/EnemiesNav/Scripts/NavMeshAgentMoveTesting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+public class NavMeshAgentMoveTesting :  MonoBehaviour, IPointerClickHandler
+{
+    private NavMeshAgent navMeshAgent;
+
+    private void Start()
+    {
+        PlayerMouseClickHandler.PlayerClickForPath +=  Move;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public void Move(Vector2 target)
+    {
+        navMeshAgent.SetDestination(target);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Клик по герою, без рейкастов!");
+    }
+}
