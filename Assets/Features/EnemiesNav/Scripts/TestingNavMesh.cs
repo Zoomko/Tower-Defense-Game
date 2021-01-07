afa846383d4cf14cbf9cd67b9db23e5b249bb300
Королев Даниил <kdvikt@gmail.com>
2021-01-07 22:11:58 +0400

    Обнаружены странности перспективной камеры
    Преобразование в мировые координаты не работало.
    Использовать ортографическую.

---
+++ Assets/Features/EnemiesNav/Scripts/TestingNavMesh.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+public class TestingNavMesh : MonoBehaviour
+{
+    public NavMeshAgent NavMeshAgent;
+
+    public float Z;
+
+    private void Start()
+    {
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = Camera.main.nearClipPlane;
+            var worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+
+            Debug.Log(pos);
+            Debug.Log(worldPosition);
+            Move(pos);
+        }
+    }
+
+    public void Move(Vector2 target)
+    {
+       // NavMeshAgent.SetDestination(target);
+    }
+}
