3a09cac4d4a229a1835d2b14e91bf951747d31b8
Misha <zagajnovmikhail@gmail.com>
2021-01-08 06:19:12 +0400

    Релизованно: - Шаблон башни (ScriptableObjects) - Префаб башни

---
+++ Assets/Main/Scripts/Grid/ArrangementTool.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+namespace TowerDefense.Grid.Arrangement
+{
+    [RequireComponent(typeof(Tilemap))]
+    public class ArrangementTool : MonoBehaviour
+    {
+       
+        private Tilemap _tilemapForUnits;
+        // Start is called before the first frame update
+        void Awake()
+        {
+            _tilemapForUnits = GetComponent<Tilemap>();
+        }
+
+        private void OnMouseDown()
+        {
+            // get mouse click's position in 2d plane
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+           
+
+            // convert mouse click's position to Grid position
+            GridLayout gridLayout = transform.parent.GetComponentInParent<GridLayout>();            
+            Vector3Int cellPosition = gridLayout.WorldToCell(mousePosition);
+
+            Debug.Log(cellPosition);
+        }
+    }
+}
---
+++ Assets/Main/Scripts/Units/Towers/Tower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TowerDefense.ScriptableObjects.Towers;
+using UnityEngine;
+
+namespace TowerDefense.Units.Tower
+{
+    public class Tower : MonoBehaviour
+    {
+        [SerializeField]
+        private TowerInfo _info;
+        public TowerInfo Info { get => _info; 
+            set 
+            { _info = value;
+              InitTower();
+            } 
+        }
+
+        private SpriteRenderer _spriteRenderer;
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            if (_info != null)
+                InitTower();
+        }
+
+        private void InitTower()
+        {
+            _spriteRenderer.sprite = _info.Image;
+        }
+        
+    }
+}
